31e9b5d5e6a3a6fead64fa5a43af2aeededf1cec
Åsmund Skomedal <Asmund.Skomedal@ife.no>
2019-01-22 13:50:46 +0100

    Added a little to the documentation of compare_strings

--- pv_string_performance_monitoring.py
+++ pv_string_performance_monitoring.py
@@ -8,19 +8,28 @@ Created on Thu Jan 17 12:19:02 2019
 import pandas as pd
 import numpy as np
 
-def compare_strings(current_df):
-    ''' Compare strings of a pv system by current/power '''
+def compare_strings(input_df):
+    ''' 
+    Compare strings of a pv system by current/power
+    Inputs:
+        - input_df: currents or powers of different strings
+        
+    returns:
+        - trigger_string_performance: True if string performance < i'th percentile,
+            where i are column names
+        - string_performance: gives deviation from median and mean in the period of each string
+    '''
     
     percentiles = [1,2,3,4,5,10,20,30,40,45]
     
     
-    aggregates = current_df.sum(axis=0)
+    aggregates = input_df.sum(axis=0)
     meanProd   = aggregates.mean()
     medianProd   = aggregates.median()
     
-    string_performance = pd.DataFrame(index=current_df.columns, columns=['dev_from_mean',
+    string_performance = pd.DataFrame(index=input_df.columns, columns=['dev_from_mean',
                                       'dev_from_median'], data=np.nan)
-    trigger_string_performance = pd.DataFrame(index=current_df.columns, 
+    trigger_string_performance = pd.DataFrame(index=input_df.columns, 
                                               columns=percentiles)
     
     string_performance['dev_from_mean'] = aggregates-meanProd
